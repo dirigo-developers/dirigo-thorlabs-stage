4af40813ab370838e98619a2baf310d3f1f4f0b8
Timothy Weber <tweber@mit.edu>
2025-01-03 13:48:13 -0500

    updated unit quantities location

--- dirigo_thorlabs_stage/dirigo_thorlabs_stage.py
+++ dirigo_thorlabs_stage/dirigo_thorlabs_stage.py
@@ -1,11 +1,15 @@
 import time
+from dataclasses import dataclass
+from functools import cached_property
 
 import clr
 
-from dirigo.interfaces.stage import Stage, StageAxis
+from dirigo import units
+from dirigo.hw_interfaces.stage import MultiAxisStage, LinearStage, StageInfo
 
 
-# Import .NET resources
+
+# Import .NET resources--unfortunately autocomplete is not helpful with these
 kinesis_location = "C:\\Program Files\\Thorlabs\\Kinesis\\"
 clr.AddReference(kinesis_location + "Thorlabs.MotionControl.DeviceManagerCLI.dll")
 clr.AddReference(kinesis_location + "Thorlabs.MotionControl.GenericMotorCLI.dll")
@@ -19,17 +23,19 @@ from Thorlabs.MotionControl.Benchtop.BrushlessMotorCLI import BenchtopBrushlessM
 from System import Decimal
 
 
-class BBD102Stage(Stage):
+
+
+class BBD102Stage(MultiAxisStage):
     """
-    Communicate with Thorlabs BBD102 brushless motor controller, which normally
+    Communicate with Thorlabs BBD102 brushless motor controller, which usually
     controls MLS203, a high-speed 2-axis linear motor stage.
     
     https://www.thorlabs.com/thorproduct.cfm?partnumber=BBD102
     https://www.thorlabs.com/thorproduct.cfm?partnumber=MLS203-1
     """
-    SERIAL_NUMBER_PREFIX = "73"
+    SERIAL_NUMBER_PREFIX = "73" # code Thorlabs uses to identify this device type
 
-    def __init__(self, defaults=None):
+    def __init__(self, x_config: dict, y_config: dict, **kwargs):
         # Build the device list, required even when device is known
         DeviceManagerCLI.BuildDeviceList()
 
@@ -40,87 +46,74 @@ class BBD102Stage(Stage):
         self._controller = None
         for device_sn in connected_devices_sn:
             if not device_sn.startswith(self.SERIAL_NUMBER_PREFIX):
-                # Not a BBD102 device
+                # Not a BBD102 device, something else
                 continue
 
             try:
                 # Attempt to create and connect to a BenchtopBrushlessMotor
                 controller = BenchtopBrushlessMotor.CreateBenchtopBrushlessMotor(device_sn)
-                    
                 controller.Connect(device_sn)
-
-                self._controller = controller
-
             except Exception as e:
                 print(f"Failed to connect to device {device_sn}: {e}")
 
+            self._controller = controller
+
         if self._controller is None:
-            raise RuntimeError("BBD102 not found among connected devices.")
+            raise RuntimeError(
+                "BBD102 not found among available devices. Check that it is "
+                "connected and not in used by another program."
+            )
 
-        self._x_axis = BBD102Axis(
-            self._controller, 
-            channel_number=1, 
-            defaults=defaults
-        ) 
-
-        self._y_axis = BBD102Axis(
-            self._controller, 
-            channel_number=2,
-            defaults=defaults
-        )
+        self._x_axis = ThorlabsLinearMotor(self._controller, **x_config)
+        self._y_axis = ThorlabsLinearMotor(self._controller, **y_config)
     
     @property
     def x(self):
+        """Returns reference to x axis motor."""
         return self._x_axis
     
     @property
     def y(self):
+        """Returns reference to y axis motor."""
         return self._y_axis
     
     @property
     def z(self):
+        """No z axis motor available."""
         return None
+        # alternatively we could raise a NotImplementedError?
 
 
-class BBD102Axis(StageAxis):
-    """
-    Represents a stage single axis controlled by BBD102.
+@dataclass
+class ThorlabsLinearMotorInfo(StageInfo):
+    # inherits StageInfo's attributes and adds the following...
+    serial_number: str
+    # extend as needed
 
-    Note: this should not be instantiated alone, requires BBD102Stage.
-    """
-    HOMING_TIMEOUT = 10 # seconds
-    MOVE_TIMEOUT = 10 # seconds
-    POLLING_PERIOD = 0.050 # seconds
 
-    def __init__(self, controller, channel_number:int, defaults:dict):
-        self.channel_number = channel_number
-        self._channel = controller.GetChannel(self.channel_number) # 1-indexed channel count
-        self._prev_position = None
-        self._last_move_timestamp = None
+class ThorlabsLinearMotor(LinearStage): # alt name: Linear brushless?
+    HOME_TIMEOUT = units.Time('10 s')
+    MOVE_TIMEOUT = units.Time('10 s')
+    POLLING_PERIOD = units.Time('50 ms')
+
+    def __init__(self, stage_controller: BBD102Stage, 
+                 position_limits: dict = None, **kwargs):
+        super().__init__(**kwargs)
+
+        # position limits may be set manually in system_config.toml, or omitted to use 
+        if position_limits is None:
+            self._position_limits = None # position limit property will read limits from API
+        else:
+            self._validate_limits_dict(position_limits)
+            self._position_limits = units.PositionRange(**position_limits)
         
-        # if "distance_per_pulse" in settings: # If position quad encoders are connected
-        #     self.distance_per_pulse = to_si_units(settings["distance_per_pulse"])
-
-        #     if self.channel_number == 1:
-        #         axis_label = "x"
-        #     elif self.channel_number == 2:
-        #         axis_label = "y"
-        #     else:
-        #         raise ValueError(">2 channels not supported")
-        #     self.encoder_a:str = settings["wiring"][axis_label]["encoder_a"]
-        #     self.encoder_b:str = settings["wiring"][axis_label]["encoder_b"]
-
-        # if "trigger_out" in settings["wiring"][axis_label]:
-        #     terminal_name = settings["wiring"][axis_label]["trigger_out"]
-        #     if terminal_name[:4] == "/Dev":
-        #         terminal_name = terminal_name
-        #     elif terminal_name[:3]:
-        #         terminal_name = "/Dev1/" + terminal_name
-        #     self.trigger_out = terminal_name
-            
+        channel_number = 1 if self.axis == 'x' else 2
+        self._channel = stage_controller.GetChannel(channel_number) 
+        
         if not self._channel.IsSettingsInitialized():
             self._channel.WaitForSettingsInitialized(10000)  # 10 second timeout
-            assert self._channel.IsSettingsInitialized() is True
+            if not self._channel.IsSettingsInitialized():
+                raise RuntimeError("Failed to initialize stage axis")
 
         # Start polling and enable
         self._channel.StartPolling(int(1000*self.POLLING_PERIOD))
@@ -134,104 +127,141 @@ class BBD102Axis(StageAxis):
         motor_config = self._channel.LoadMotorConfiguration(self._channel.DeviceID)  
         device_settings = self._channel.MotorDeviceSettings
 
-        # if self.homed is False:
-        #     self.home()
+    @cached_property 
+    def device_info(self) -> ThorlabsLinearMotorInfo:
+        """Returns an object describing permanent properties of the stage."""
+        return ThorlabsLinearMotorInfo(
+            manufacturer="Thorlabs",
+            model="MLS203", # should name the actuator
+            serial_number=self._channel.DeviceID
+            # controller name, etc
+            # serial number -- self._channel.DeviceID
+        )
 
-        # step_size = 1e-3
-        # max_velocity = to_si_units(settings["velocity"])
-        # acceleration = to_si_units(settings["acceleration"])
-        # self.set_jog_parameters(step_size, max_velocity, acceleration) 
+    @cached_property
+    def position_limits(self) -> units.PositionRange:
+        if self._position_limits is None:
+            min_position = Decimal.ToDouble(
+                self._channel.AdvancedMotorLimits.LengthMinimum) / 1000
+            max_position = Decimal.ToDouble(
+                self._channel.AdvancedMotorLimits.LengthMaximum) / 1000
+            return units.PositionRange(min_position, max_position)
+        else:
+            return self._position_limits
+        
+    @property
+    def position(self) -> units.Position:
+        """The current spatial position."""
+        position = units.Position(
+            Decimal.ToDouble(self._channel.Position) / 1000
+        )
+
+        # to fix BUG: immediately after MoveTo command, Position reads exactly 0
+        if position == 0:
+            # overwrite the current invalid position with the previous known position
+            position = self._prev_position
+        else:
+            self._prev_position = position # store known position
+
+        return position
+
+    def move_to(self, position: units.Position, blocking: bool = False):
+        """
+        Initiate move to specified spatial position.
+
+        Choose whether to return immediately (blocking=False, default) or to
+        wait until finished moving (blocking=True). If the method does not
+        return for more than 10 seconds in a blocking move, will timeout.
+        """
+        # Validate move position
+        if not self.position_limits.within_range(position):
+            raise ValueError(
+                f"Requested move, ({position}) beyond limits, "
+                f"min: {self.position_limits.min}, max: min: {self.position_limits.max}"
+            )
+        
+        timeout_or_blocking = int(1000 * self.MOVE_TIMEOUT) if blocking else 0
+        
+        self._channel.MoveTo(Decimal(1000 * position), timeout_or_blocking) 
+        
+        self._last_move_timestamp = time.perf_counter() # why?
 
     @property
-    def busy(self):
+    def moving(self) -> bool:
+        """Return True if the stage axis is currently moving."""
         return self._channel.IsDeviceBusy
 
-    @property
-    def homed(self):
-        return bool(self._channel.Status.IsHomed)
-
-    def home(self, blocking=False):
-        
-        if not blocking:
-            self._channel.Home(0) # argument = 0 codes for non-blocking
-        else:
-            self._channel.Home(int(1000*self.HOMING_TIMEOUT))
-    
-    @property
-    def max_velocity(self):
-        vel_params = self._channel.GetVelocityParams()
-        v_max_mm_per_sec = Decimal.ToDouble(vel_params.MaxVelocity) 
-        return v_max_mm_per_sec / 1000 # convert to m/s
-
-    @max_velocity.setter
-    def max_velocity(self, max_velo_m_per_s:float):
-        value_mm_per_sec = max_velo_m_per_s * 1000 # convert to mm/s
-        vel_params = self._channel.GetVelocityParams()
-        vel_params.MaxVelocity = Decimal(value_mm_per_sec)
-        self._channel.SetVelocityParams(vel_params)
-
-    @property
-    def acceleration(self):
-        velo_params = self._channel.GetVelocityParams()
-        a_mm_per_sec2 = Decimal.ToDouble(velo_params.Acceleration)
-        return a_mm_per_sec2 / 1000 # convert to m/s^2
-
-    @acceleration.setter
-    def acceleration(self, accel_m_per_s2:float):
-        value_mm_per_sec2 = accel_m_per_s2 * 1000
-        vel_params = self._channel.GetVelocityParams()
-        vel_params.Acceleration = Decimal(value_mm_per_sec2)
-        self._channel.SetVelocityParams(vel_params)
-
-    @property
-    def position(self):
-        p_mm = Decimal.ToDouble(self._channel.Position)
-        p = p_mm / 1000 # convert to meters
-
-        # to fix BUG: immediately after issuing MoveTo command, reads 0.0
-        if p == 0.0:
-            # overwrite invalid position (0.0) with the last known postiion
-            p = self._prev_position
-        else:
-            self._prev_position = p
-
-        return p
-
-    def move_to_position(self, position:float, blocking:bool=False):
-        # Check within limits
-        if not (self.min_position <= position <= self.max_position):
-            raise ValueError(f"Requested move beyond limits ({position})")
-
-        new_pos_mm = Decimal(position * 1000) # convert to mm
-        if not blocking:
-            self._channel.MoveTo(new_pos_mm, int(0)) # 2nd arg=0 means non-blocking
-        else:
-            self._channel.MoveTo(new_pos_mm, int(1000*self.MOVE_TIMEOUT)) 
-            
-        self._last_move_timestamp = time.perf_counter() # TODO, how is this used?
-
     def stop(self):
+        """Halts motion."""
         self._channel.StopImmediate()
 
-    @property
-    def min_position(self):
-        min_position_mm = Decimal.ToDouble(
-            self._channel.AdvancedMotorLimits.LengthMinimum)
-        return min_position_mm / 1000 # convert to mm
+    def home(self, blocking: bool = False):
+        """
+        Initiate homing. 
+        
+        Choose whether to return immediately (blocking=False, default) or to
+        wait until finished homing (blocking=True). If the method does not
+        return for more than 10 seconds in a blocking move, will timeout.
+        """
+        timeout_or_blocking = int(1000 * self.MOVE_TIMEOUT) if blocking else 0
+        self._channel.Home(timeout_or_blocking) # arg=0 for non-blocking
 
     @property
-    def max_position(self):
-        max_position_mm = Decimal.ToDouble(
-            self._channel.AdvancedMotorLimits.LengthMaximum)
-        return max_position_mm / 1000 # convert to mm
+    def homed(self) -> bool:
+        """Return whether the stage has been home."""
+        return self._channel.Status.IsHomed
 
     @property
-    def home_position(self):
-        return (self.max_position + self.min_position) / 2
+    def max_velocity(self) -> units.Velocity:
+        """
+        Return the current maximum velocity setting.
+
+        Note that this is the imposed velocity limit for moves. It is not
+        necessarily the maximum attainable velocity for this stage.
+        """
+        vel_params = self._channel.GetVelocityParams()
+        v_max = Decimal.ToDouble(vel_params.MaxVelocity) 
+        return units.Velocity(v_max / 1000)  # API uses velocity in mm/s
+
+    @max_velocity.setter
+    def max_velocity(self, new_velocity: units.Velocity):
+        # TODO: validate
+        vel_params = self._channel.GetVelocityParams()
+        vel_params.MaxVelocity = Decimal(new_velocity * 1000) # API uses velocity in mm/s
+        self._channel.SetVelocityParams(vel_params)
+
+    @property
+    def acceleration(self) -> units.Acceleration:
+        """
+        Return the acceleration used during ramp up/down phase of move.
+        """
+        vel_params = self._channel.GetVelocityParams()
+        acceleration = Decimal.ToDouble(vel_params.Acceleration)
+        return units.Acceleration(acceleration / 1000) # API uses velocity in mm/s^2
+
+    @acceleration.setter
+    def acceleration(self, new_acceleration: units.Acceleration):
+        # TODO, validate
+        vel_params = self._channel.GetVelocityParams()
+        vel_params.Acceleration = Decimal(new_acceleration * 1000) # API uses velocity in mm/s^2
+        self._channel.SetVelocityParams(vel_params)
 
 
+
+
+
+# For testing
 if __name__ == "__main__":
 
-    stage = BBD102Stage()
+    x_config = {
+        "axis": "x",
+        "position_limits": {"min": "0 mm", "max": "85 mm"} 
+    }
+    y_config = {
+        "axis": "y",
+        "position_limits": {"min": "0 mm", "max": "55 mm"} 
+    }
+
+    stage = BBD102Stage(x_config, y_config)
 
     None
